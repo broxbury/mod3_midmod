0177d4a0a43c28244fb818ec14e49d4b17fcb703
Brian Roxbury <roxburybrian@gmail.com>
2020-05-21 09:49:57 -0600

    Fetch functionality

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(res => res.json())
+    .then(info => {
+      const promises = info.map(reservation => {
+        return {
+          id: reservation.id,
+          name: reservation.name,
+          date: reservation.date,
+          time: reservation.time,
+          number: reservation.number
+        }
+      })
+      Promise.all(promises).then(completedReservations => this.setState({reservations: completedReservations}))
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +39,10 @@ class App extends Component {
       </div>
     )
   }
+
+  
+
+ 
 }
 
 export default App;
